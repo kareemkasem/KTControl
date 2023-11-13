01895b570a4823aee5457c85105ece313ba89dc0
kareem kasem <kareemkasemn@gmail.com>
2023-11-13 19:24:47 +0200

    added a front page for the site major css overhaul extracted navbar and head to a shared folder in the views update the function responsible for employee expansion

--- public/js/employee.js
+++ public/js/employee.js
@@ -5,14 +5,14 @@ const deleteEmployeeModal = document.querySelector("#delete-employee-modal");
 const deleteEmployeeButton = document.querySelector("#delete-employee-button");
 const confirmDeleteEmployeeButton = document.querySelector("#confirm-delete-employee-button");
 const cancelDeleteEmployeeButton = document.querySelector("#cancel-delete-employee-button");
-const updateEmployeePasswordModal = document.querySelector("#update-employee-password-modal")
-const changeEmployeePasswordButton = document.querySelector("#change-employee-password-button")
-const cancelChangeEmployeePasswordButton = document.querySelector("#cancel-change-employee-password-button")
+const updateEmployeePasswordModal = document.querySelector("#update-employee-password-modal");
+const changeEmployeePasswordButton = document.querySelector("#change-employee-password-button");
+const cancelChangeEmployeePasswordButton = document.querySelector("#cancel-change-employee-password-button");
 
 if (employeeList)
 	employeeList.addEventListener("click", (e) => {
-		if (e.target.className === "employee-details") {
-			const id = e.target.id;
+		const id = e.target.dataset.id;
+		if (id) {
 			window.location.href = `${window.location.origin}/employee/${id}`;
 		}
 	});
@@ -35,7 +35,7 @@ if (confirmDeleteEmployeeButton)
 			.catch((e) => {
 				throw new Error(e);
 			});
-		// ! note: When you use fetch with the delete method, it doesn’t automatically follow redirects. The server response is received and processed by the browser, but your JavaScript code doesn’t get the redirected page content. that's why you HAVE TO redirect using the fronend
+		// ! note: When you use fetch with the delete method, it doesn’t automatically follow redirects. The server response is received and processed by the browser, but your JavaScript code doesn’t get the redirected page content. that's why you HAVE TO redirect using the frontend
 	});
 
 if (cancelDeleteEmployeeButton)
@@ -43,13 +43,13 @@ if (cancelDeleteEmployeeButton)
 		deleteEmployeeModal.style.display = "none";
 	});
 
-if(changeEmployeePasswordButton)
-	changeEmployeePasswordButton.addEventListener("click", ()=> {
-		updateEmployeePasswordModal.style.display = "flex"
-	})
+if (changeEmployeePasswordButton)
+	changeEmployeePasswordButton.addEventListener("click", () => {
+		updateEmployeePasswordModal.style.display = "flex";
+	});
 
-if(cancelChangeEmployeePasswordButton)
-	cancelChangeEmployeePasswordButton.addEventListener("click",(e)=>{
-		e.preventDefault() // ! EVERY BUTTON SUBMITS THE FORM BY DEFAULT
-		updateEmployeePasswordModal.style.display = "none"
-	})
+if (cancelChangeEmployeePasswordButton)
+	cancelChangeEmployeePasswordButton.addEventListener("click", (e) => {
+		e.preventDefault(); // ! EVERY BUTTON SUBMITS THE FORM BY DEFAULT
+		updateEmployeePasswordModal.style.display = "none";
+	});
