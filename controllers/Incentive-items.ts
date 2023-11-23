c61a2a3df4308f77713149db82f0040a39aeca4c
kareem kasem <kareemkasemn@gmail.com>
2023-11-23 19:10:07 +0200

    added add incentive item functionality changed numbers in price and incentive to floats

--- controllers/Incentive-items.ts
+++ controllers/Incentive-items.ts
@@ -1,109 +1,111 @@
-import {Request, Response} from "express";
-import {COLLECTIONS} from "../database";
-import {ObjectId} from "mongodb";
-import {IncentiveItem, IncentiveItemFormInput} from "../types";
-import {IncentiveItemSchema} from "../models/IncentiveItem";
-import {parseDate} from "../utils/date-parser";
+import { Request, Response } from "express";
+import { COLLECTIONS } from "../database";
+import { ObjectId } from "mongodb";
+import { IncentiveItem, IncentiveItemFormInput } from "../types";
+import { IncentiveItemSchema } from "../models/IncentiveItem";
+import { parseDate } from "../utils/date-parser";
 
 // GET /incentive/items
 export async function getAllIncentiveItems(req: Request, res: Response) {
-    try {
-        const items = await COLLECTIONS.incentiveItems.find().sort({validTill: "desc"}).toArray();
-        res.status(200).render("Incentive/items", {items});
-    } catch (error) {
-        res.status(500).send({message: (error as Error).message});
-    }
+	try {
+		const items = await COLLECTIONS.incentiveItems
+			.find()
+			.sort({ validTill: "desc" })
+			.toArray();
+		res.status(200).render("Incentive/items", { items });
+	} catch (error) {
+		res.status(500).send({ message: (error as Error).message });
+	}
 }
 
 // GET /incentive/items/:id
 export async function getUpdateIncentiveItem(
-    req: Request<{ id: string }>,
-    res: Response
+	req: Request<{ id: string }>,
+	res: Response
 ) {
-    const id = req.params.id;
-    try {
-        const item = await COLLECTIONS.incentiveItems.findOne({
-            _id: new ObjectId(id),
-        });
-        if (!item) {
-            res.status(404).send({message: "item not found"});
-            return;
-        }
-        res.status(200).render("Incentive/update-item", {
-            ...item,
-            validTill: parseDate(item.validTill)
-        });
-    } catch (error) {
-        res.status(500).send({message: (error as Error).message});
-    }
+	const id = req.params.id;
+	try {
+		const item = await COLLECTIONS.incentiveItems.findOne({
+			_id: new ObjectId(id),
+		});
+		if (!item) {
+			res.status(404).send({ message: "item not found" });
+			return;
+		}
+		res.status(200).render("Incentive/update-item", {
+			...item,
+			validTill: parseDate(item.validTill),
+		});
+	} catch (error) {
+		res.status(500).send({ message: (error as Error).message });
+	}
 }
 
-// * Testing ✅
 // GET /incentive/items/create-item
 export function getCreateIncentiveItem(req: Request, res: Response) {
-    res.status(200).render(""); // TODO: add the view
+	res.status(200).render("Incentive/create-item.ejs");
 }
 
-// * Testing ✅
 // POST /incentive/items
 export async function createIncentiveItem(
-    req: Request<{}, {}, IncentiveItemFormInput>,
-    res: Response
+	req: Request<{}, {}, IncentiveItemFormInput>,
+	res: Response
 ) {
-    const {incentive, name, price, validTill} = req.body;
-    const incentiveItem: IncentiveItem = {
-        name,
-        price: parseInt(price),
-        incentive: parseInt(incentive),
-        validTill: new Date(validTill)
-    }
-    const {error} = IncentiveItemSchema.validate(incentiveItem);
-    if (error) {
-        res.status(400).send({message: error.message});
-        return;
-    }
+	const { incentive, name, price, validTill } = req.body;
+	const incentiveItem: IncentiveItem = {
+		name,
+		price: parseFloat(price),
+		incentive: parseFloat(incentive),
+		validTill: new Date(validTill),
+	};
+	const { error } = IncentiveItemSchema.validate(incentiveItem);
 
-    try {
-        await COLLECTIONS.incentiveItems.insertOne(incentiveItem);
-        res.status(200).redirect("/");
-    } catch (error) {
-        res.status(500).send({message: (error as Error).message});
-    }
+	if (error) {
+		res.status(400).send({ message: error.message });
+		return;
+	}
+
+	try {
+		await COLLECTIONS.incentiveItems.insertOne(incentiveItem);
+		res.status(200).redirect("/incentive/items");
+	} catch (error) {
+		res.status(500).send({ message: (error as Error).message });
+	}
 }
 
 // POST /incentive/items/:id
 export async function updateIncentiveItem(
-    req: Request<{ id: string }, {}, IncentiveItemFormInput>,
-    res: Response
+	req: Request<{ id: string }, {}, IncentiveItemFormInput>,
+	res: Response
 ) {
-    const id = req.params.id;
-    let {name, price, incentive, validTill} = req.body;
-    const incentiveItem: IncentiveItem = {
-        name,
-        price: parseInt(price),
-        incentive: parseInt(incentive),
-        validTill: new Date(validTill)
-    }
+	const id = req.params.id;
+	let { name, price, incentive, validTill } = req.body;
+	const incentiveItem: IncentiveItem = {
+		name,
+		price: parseFloat(price),
+		incentive: parseFloat(incentive),
+		validTill: new Date(validTill),
+	};
 
-    const {error} = IncentiveItemSchema.validate(incentiveItem);
-    if (error) {
-        res.status(400).send({message: error.message});
-        return;
-    }
+	const { error } = IncentiveItemSchema.validate(incentiveItem);
+	if (error) {
+		res.status(400).send({ message: error.message });
+		return;
+	}
 
-    try {
-        const {matchedCount, modifiedCount} =
-            await COLLECTIONS.incentiveItems.updateOne(
-                {_id: new ObjectId(id)},
-                {$set: {...incentiveItem}}
-            );
-        if (matchedCount !== 1 && modifiedCount !== 1) {
-            res.status(404).send({message: "item not found"});
-            return;
-        } else {
-            res.status(201).redirect(`/incentive/items`);
-        }
-    } catch (error) {
-        res.status(500).send({message: (error as Error).message + " nigga"});
-    }
+	try {
+		const { matchedCount, modifiedCount } =
+			await COLLECTIONS.incentiveItems.updateOne(
+				{ _id: new ObjectId(id) },
+				{ $set: { ...incentiveItem } }
+			);
+		if (matchedCount !== 1 && modifiedCount !== 1) {
+			res.status(404).send({ message: "item not found" });
+			return;
+		} else {
+			res.status(201).redirect(`/incentive/items`);
+		}
+	} catch (error) {
+		res.status(500).send({ message: (error as Error).message });
+	}
 }
