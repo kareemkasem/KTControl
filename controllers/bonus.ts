d928074102b8e1423e2f18b4a81d7827eb4aa293
kareem kasem <kareemkasemn@gmail.com>
2023-12-09 10:38:23 +0200

    view ongoing transactions add new entry

--- controllers/bonus.ts
+++ controllers/bonus.ts
@@ -2,7 +2,39 @@ import { Request, Response } from "express";
 import { Bonus, BonusFormInput, Employee } from "../types";
 import { BonusSchema } from "../models/Bonus";
 import { db } from "../database";
+import { monthParser } from "../utils/date-parser";
+import { ObjectId } from "mongodb";
 
+// GET /bonuses
+export async function getMainPage(req: Request, res: Response) {
+	const currentMonth = monthParser();
+	try {
+		const data = await db.bonuses.find({ month: currentMonth }).toArray();
+		res.status(200).render("Bonuses/index.ejs", { data });
+	} catch (error) {
+		res.status(500).send({ message: (error as Error).message });
+	}
+}
+
+// GET /bonuses/new
+export async function getCreateBonusPage(req: Request, res: Response) {
+	try {
+		const { employees } = (
+			await db.employees
+				.aggregate<{ _id: ObjectId; employees: string[] }>([
+					{ $match: {} },
+					{ $project: { name: 1 } },
+					{ $group: { _id: {}, employees: { $addToSet: "$name" } } },
+				])
+				.toArray()
+		)[0];
+		res.status(200).render("Bonuses/add.ejs", { employees });
+	} catch (error) {
+		res.send({ error: (error as Error).message });
+	}
+}
+
+// POST /bonuses
 export async function createBonus(
 	req: Request<{}, {}, BonusFormInput>,
 	res: Response
@@ -10,6 +42,7 @@ export async function createBonus(
 	let bonus: Bonus = {
 		...req.body,
 		amount: +req.body.amount,
+		month: monthParser(),
 	};
 	const { value, error } = BonusSchema.validate(bonus);
 	if (error) {
@@ -34,7 +67,7 @@ export async function createBonus(
 
 	try {
 		await db.bonuses.insertOne(bonus);
-		res.status(201).redirect("/");
+		res.status(201).redirect("/bonuses");
 		return;
 	} catch (error) {
 		res.status(500).send({ error: (error as Error).message });
--- routes/bonuses.ts
+++ routes/bonuses.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { createBonus } from "../controllers/bonus";
+import {
+	createBonus,
+	getCreateBonusPage,
+	getMainPage,
+} from "../controllers/bonus";
 
 const Router = express.Router();
 
-Router.get("/", () => {});
+Router.get("/", getMainPage);
+Router.get("/new", getCreateBonusPage);
 Router.post("/", createBonus);
 Router.delete("/", () => {});
 Router.get("history", () => {});
